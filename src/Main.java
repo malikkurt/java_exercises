f8dd53bd2bda7157509defed08a0e875c8a952b0
malikkurt <malikkurt612@gmail.com>
2022-10-07 19:33:22 +0300

    method

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         Products Laptop = new Products();
-        Laptop.name = "Huawei";
-        Laptop.color = "Black";
-        Laptop.id = 1;
 
-        System.out.println(Laptop.id);
-        System.out.println(Laptop.name);
-        System.out.println(Laptop.color);
-
+        Laptop.screenprnt("Huawei",1,"Black");
     }
 }
--- src/Products.java
+++ src/Products.java
@@ -1,5 +1,10 @@
 public class Products {
-    int id;
-    String name;
-    String color;
+
+
+    public void screenprnt(String name,int id ,String color){
+        System.out.println(name);
+        System.out.println(id);
+        System.out.println(color);
+
+    }
 }
