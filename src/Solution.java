46cd705a0cf97f449df8b6a95b359e4c728455b1
malikkurt <malikkurt612@gmail.com>
2022-10-21 20:20:10 +0300

    HackerRank | Java Stdin and Stdout 2

--- src/Solution.java
+++ src/Solution.java
@@ -1,33 +1,20 @@
-import java.util.*;
-
+import java.util.Scanner;
 
 public class Solution {
 
-
-    private static final Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
-        int N = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        Scanner scan = new Scanner(System.in);
 
-        scanner.close();
-
-        if(N%2==1){
-            System.out.println("Weird");
-        }
-        else{
-            if(N>=2 && N<=5){
-                System.out.println("Not Weird");
-            }
-            else if (N>=6 && N<=20){
-                System.out.println("Weird");
-            }
-            else{
-                System.out.println("Not Weird");
-            }
-        }
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
 
 
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
 
     }
 }
