b86ff25a3b232cf72f42d09de9dd710ff0a9dd4e
malikkurt <malikkurt612@gmail.com>
2022-10-31 14:35:35 +0300

    HackerRank | Java Stdin and Output Formatting

--- src/Solution.java
+++ src/Solution.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++){
+            String s1=sc.next();
+            int x=sc.nextInt();
 
-        int i = scan.nextInt();
-        double d = scan.nextDouble();
-        scan.nextLine();
-        String s = scan.nextLine();
-
-
-
-        System.out.println("String: " + s);
-        System.out.println("Double: " + d);
-        System.out.println("Int: " + i);
+            System.out.printf("%-15s%03d%n",s1,x);
+        }
+        System.out.println("================================");
 
     }
 }
+
+
+
